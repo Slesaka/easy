c2b286c73a879c0fe6961255879b69e6c76e88c7
Slesaka <161320208+Slesaka@users.noreply.github.com>
2024-03-11 13:47:44 +0300

    Add files via upload

--- nabor3.cpp
+++ nabor3.cpp
@@ -35,7 +35,7 @@ int itc_str(int a, int b, int c){
     if (a <= 0 || b <= 0 || c <= 0){
         return -1;
     }
-    if (a + b <= c; || a + c <= b || b + c <= a){
+    if (a + b <= c || a + c <= b || b + c <= a){
         return -1;
     }
     int p = (a + b + c) / 2;
